2734a0c48b91d90954c054d67496096f63e8c2f6
Marcushe31 <marcusrhernandez99@gmail.com>
2026-05-31 17:19:42 -0700

    Train demo on Frankenstein corpus

--- src/main/java/com/marc/languagemodel/Main.java
+++ src/main/java/com/marc/languagemodel/Main.java
@@ -1,13 +1,14 @@
 package com.marc.languagemodel;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        String text = "The dog ran home. The dog ate food. The cat ran home. "
-                + "The cat ate food. The dog slept. The cat slept. "
-                + "A student wrote code. A student tested code. "
-                + "The model learned words. The model generated words.";
+    public static void main(String[] args) throws IOException {
+        String text = Files.readString(Path.of("src/main/resources/frankenstein.txt"));
+        text = cleanGutenbergText(text);
 
         Tokenizer tokenizer = new Tokenizer();
         List<String> tokens = tokenizer.tokenize(text);
@@ -21,11 +22,30 @@ public class Main {
 
         TextGenerator generator = new TextGenerator(model);
 
-        // basic demo for now, just to prove the whole pipeline works
+        // trained on frankenstein, then tested on the last chunk of tokens
+        System.out.println("corpus: Frankenstein by Mary Shelley");
         System.out.println("total tokens: " + tokens.size());
         System.out.println("training tokens: " + trainTokens.size());
         System.out.println("validation tokens: " + validationTokens.size());
         System.out.println("generated text: " + generator.generateSentence(20));
         System.out.println("validation nll: " + model.negativeLogLikelihood(validationTokens));
+    }
+
+    private static String cleanGutenbergText(String text) {
+        int start = text.indexOf("*** START OF THE PROJECT GUTENBERG EBOOK");
+        int end = text.indexOf("*** END OF THE PROJECT GUTENBERG EBOOK");
+
+        if (start != -1) {
+            start = text.indexOf("***", start + 3);
+            if (start != -1) {
+                text = text.substring(start + 3);
+            }
+        }
+
+        if (end != -1) {
+            text = text.substring(0, end);
+        }
+
+        return text;
     }
 }
